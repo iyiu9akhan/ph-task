648e9393417338fc392e202e6f3dd8685c604841
iyiu9akhan <iyiu9akhan@gmail.com>
2026-07-16 21:00:25 +0600

    module8 practice Task

---
+++ module-8/script.js
@@ -0,0 +1,19 @@
+let firstPrice = 6;
+let secondPrice = 3;
+let thirdPrice = 6;
+
+// double & triple equal start
+console.log("(1) using == :", firstPrice == secondPrice);
+console.log("(2) using == :", firstPrice == thirdPrice);
+
+console.log("(3) using === :", firstPrice === secondPrice);
+console.log("(4) using === :", firstPrice === thirdPrice);
+// double & triple equal end
+
+// Not Equal & Strict Not Equal start
+console.log("(5) using != :", firstPrice != secondPrice);
+console.log("(6) using != :", firstPrice != thirdPrice);
+
+console.log("(7) using !== :", firstPrice !== secondPrice);
+console.log("(8) using !== :", firstPrice !== thirdPrice);
+// Not Equal & Strict Not Equal end
