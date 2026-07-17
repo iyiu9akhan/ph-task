497067a42356e89fbaf594894fd87f491b476bd8
iyiu9akhan <iyiu9akhan@gmail.com>
2026-07-17 20:29:26 +0600

    forLoop practice

---
+++ module-8/guessTheNmbr.js
@@ -0,0 +1,11 @@
+let firstNumber = "5";
+let secondNumber = 5;
+
+// first guess : "5" == 5 ;
+// answer : true ;
+
+// second guess : "5" === 5 ;
+// answer : false ;
+
+console.log("first guess :", firstNumber == secondNumber);
+console.log("second guess :", firstNumber === secondNumber);
